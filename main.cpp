bd3e0433b8cf63b2fdb0768be0d7ee7edd59c987
Aman <amangrand@gmail.com>
2020-07-23 04:22:04 +0530

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+#include <ext/pb_ds/detail/standard_policies.hpp>
+using namespace __gnu_pbds; 
+using namespace std;
+
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define REP(i, n) for (i=0; i < n; i++)
+#define ll long long
+#define ull unsigned long long
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define Itr(it,a) for(auto it = a.begin(); it != a.end(); it++)
+#define ok order_of_key
+#define fo find_by_order
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pi> vpi;
+typedef vector<pll> vpll;
+typedef map<int,int> mii;
+typedef map<string,int>msi;
+typedef map<ll,ll>mll;
+typedef priority_queue<int,vector<int>,greater<int>> MinPq;
+typedef priority_queue<int> MaxPq;
+typedef unordered_set<int> unset;
+typedef unordered_map<int,int> unmap;
+typedef tree<pair<int,int>, null_type, less<pair<int,int>>, rb_tree_tag,tree_order_statistics_node_update> pbds; 
+const char nl ='\n';
+const long long MOD = 1e9 + 7;
+const long long INF = 1e9 + 7;
+
+void IO(){
+  #ifndef ONLINE_JUDGE  freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+  #endif
+}
+
+// ======
+
+
+void decode(){
+     
+}
+
+
+int main(){
+       IO(); 
+       ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+       int T = 1;
+       //cin >> T;
+       while(T--) {
+           decode();
+       }
+  return 0;
+}
